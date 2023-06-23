5b4a47f215948416b98062e21e42c07027a5d4d1
p3wv <memiarzyskrzynia@gmail.com>
2023-06-23 14:14:01 +0200

    firebase integration

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD6k3nIjIAfmhVbLKJWDz8qylxpJOvI0p4",
+    authDomain: "rms-test-d41de.firebaseapp.com",
+    projectId: "rms-test-d41de",
+    storageBucket: "rms-test-d41de.appspot.com",
+    messagingSenderId: "1084831064966",
+    appId: "1:1084831064966:web:43f0a1b7a15841436a4207"
+};
+
+  const app = initializeApp(firebaseConfig);
