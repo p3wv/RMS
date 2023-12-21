7128277a16e3c540d52db7d688f479d29cd0e32d
p3wv <memiarzyskrzynia@gmail.com>
2023-12-21 02:47:00 +0100

    idk

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-
-from flask import Flask, render_template, url_for, redirect, request, session
-from flask_socketio import join_room, leave_room, send, SocketIO
+from flask import Flask, render_template, url_for, redirect, request, flash, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
-import random
-from string import ascii_uppercase
+from flask_socketio import SocketIO, emit
+# from werkzeug.urls import url_encode
 
 
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'thisisasecretkey'
 socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = 3600
+app.config['SESSION_PROTECTION'] = 'strong'
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app)
@@ -25,7 +26,7 @@ login_manager.login_view = "login"
 
 @login_manager.user_loader
 def load_user(user_id):
-     return User.query.get(int(user_id))
+     return User.session.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
@@ -66,6 +67,16 @@ with app.app_context():
 def home():
     return render_template('home.html')
 
+@socketio.on('connect')
+def handle_connect():
+    username = request.args.get('username')
+    emit('user_connected', {'username': username}, broadcast=True)
+
+@socketio.on('message')
+def handle_message(message):
+    username = request.args.get('username')
+    emit('message', {'username': username, 'message': message}, broadcast=True)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -86,7 +97,7 @@ def dashboard():
 @login_required
 def logout():
      logout_user()
-     return redirect(url_for('login'))
+     return redirect(url_for('home'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -102,5 +113,4 @@ def register():
     return render_template('register.html', form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    socketio.run(app, debug=True)
+    socketio.run(app)
