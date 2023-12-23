1f6a2702f5927f7775d02e254c9dcdc39dc07279
p3wv <memiarzyskrzynia@gmail.com>
2023-12-23 07:49:28 +0100

    auth dir

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    
 
     db.init_app(app)
 
@@ -17,5 +18,7 @@ def create_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
