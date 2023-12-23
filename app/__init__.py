7f5beba3350c97bc3a2027060b716ea47ba73d96
p3wv <memiarzyskrzynia@gmail.com>
2023-12-23 07:54:12 +0100

    models.py - currently logged user query (user_loader)

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,18 @@ from flask import Flask, render_template
 # from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login' # type: ignore
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    login_manager.init_app(app)
     
 
     db.init_app(app)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from app.RMS import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Permission:
     FOLLOW = 1
