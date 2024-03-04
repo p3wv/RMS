5fed35f711c496931188135c215dd4ca9ce40dbd
p3wv <memiarzyskrzynia@gmail.com>
2024-03-04 02:49:12 +0100

    .

--- app/models.py
+++ app/models.py
@@ -140,3 +140,16 @@ class AnonymousUser(AnonymousUserMixin):
         return False
 
 login_manager.anonymous_user = AnonymousUser
+
+
+#TODO:
+
+# class Order(UserMixin, db.Model):
+#     __tablename__ = 'orders'
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(64), unique=True, index=True)
+#     username = db.Column(db.String(64), unique=True, index=True)    
+#     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+#     password_hash = db.Column(db.String(128))
+#     confirmed = db.Column(db.Boolean, default=False)
+#     name = db.Column(db.String(64))
