47c804b636e9f4a31934e1fb6f1aed28734966f7
p3wv <memiarzyskrzynia@gmail.com>
2024-03-04 03:10:35 +0100

    added 'orders' table

--- app/models.py
+++ app/models.py
@@ -144,12 +144,9 @@ login_manager.anonymous_user = AnonymousUser
 
 #TODO:
 
-# class Order(UserMixin, db.Model):
-#     __tablename__ = 'orders'
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(64), unique=True, index=True)
-#     username = db.Column(db.String(64), unique=True, index=True)    
-#     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-#     password_hash = db.Column(db.String(128))
-#     confirmed = db.Column(db.Boolean, default=False)
-#     name = db.Column(db.String(64))
+class Order(UserMixin, db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    address = db.Column(db.String(64))
+    name = db.Column(db.String(64))
